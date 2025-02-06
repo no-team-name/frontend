0c4f914f7f4d86e2a72c704d65fc231676cb4e8e
ensoary@gmail.com <ensoary@gmail.com>
2025-02-06 18:21:35 +0900

    feat: 팀원소개 페이지 완성

--- src/pages/AboutUs.jsx
+++ src/pages/AboutUs.jsx
@@ -1,5 +1,11 @@
 import React, { useState } from 'react';
 import MainHeader from '../components/common/MainHeader';
+import TeamMemberCard from '../components/common/TeamMemberCard';
+import yubinemonImg from '../assets/park.png';
+import choijinguImg from '../assets/choi.png';
+import simjinguImg from '../assets/sim.png';
+import jungjinguImg from '../assets/jung.png';
+import leejinguImg from '../assets/lee.png';
 
 const AboutUs = ({
   openLoginModal,
@@ -30,6 +36,49 @@ const AboutUs = ({
       <div className="absolute inset-0 flex items-center justify-center">
         <div className="w-[700px] h-[700px] bg-gradient-to-r from-red-300 via-purple-300 to-blue-300 blur-3xl opacity-50 rounded-full"></div>
       </div>
+      {/* 콘텐츠 영역 (배경 위에 표시) */}
+      <div className="relative z-10 p-4">
+          {/* 프로젝트 소개 */}
+          <div className="text-center mb-8">
+            <h1 className="text-3xl font-bold">프로젝트 팀원 소개</h1>
+          </div>
+
+          {/* 팀원 카드 (3개 + 2개) */}
+          <div style={{ display: 'flex', gap: '20px', marginBottom: '20px' }}>
+            <TeamMemberCard
+              imageUrl={leejinguImg}
+              memberName="최요셉"
+              github="https://github.com/yoda-yoda/yoda-yoda"
+              email="aslopeys941@gmail.com"
+            />
+            <TeamMemberCard
+              imageUrl={yubinemonImg}
+              memberName="박유빈"
+              github="https://github.com/yyubin"
+              email="hazing0910@gmail.com"
+            />
+            <TeamMemberCard
+              imageUrl={jungjinguImg}
+              memberName="정석환"
+              github="https://github.com/SeokHwanJeongKR"
+              email="jsver12@gmail.com"
+            />
+          </div>
+          <div style={{ display: 'flex', gap: '20px' }}>
+            <TeamMemberCard
+              imageUrl={simjinguImg}
+              memberName="이시현"
+              github="https://github.com/Philo-LEE"
+              email="tlgus7777@gmail.com"
+            />
+            <TeamMemberCard
+              imageUrl={choijinguImg}
+              memberName="심윤보"
+              github="https://github.com/Waffle-ens/Project_Repository1"
+              email="ensoary@gmail.com"
+            />
+          </div>
+        </div>
       </div>
     </div>
   );
